cabf6fc8ca7609c9aa603e0ad5f7dc1f36efb044
eli5it <eli.davis1104@gmail.com>
2025-08-30 22:29:46 -0400

    commenced work on frontend

--- todo/frontend/src/App.tsx
+++ todo/frontend/src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import type { FormEvent } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function submitHandler(e: FormEvent) {
+    e.preventDefault();
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="wrapper">
+        <header>
+          <h1 className="text-center bold text-header bg-white">Todo App</h1>
+        </header>
+        <main className="">
+          <form className="todo-form">
+            <input placeholder="Add a new todo..." type="text" />
+            <button
+              className="submit-button"
+              type="submit"
+              aria-label="Add todo"
+              onSubmit={submitHandler}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-plus-icon lucide-plus"
+              >
+                <path d="M5 12h14" />
+                <path d="M12 5v14" />
+              </svg>
+            </button>
+          </form>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
