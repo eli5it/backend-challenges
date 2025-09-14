2d406733cc396d53cbce5b0d9276f922e199c2cd
eli5it <eli.davis1104@gmail.com>
2025-09-14 18:49:51 -0400

    updated auth flow

--- todo/frontend/src/layouts/AuthLayout.tsx
+++ todo/frontend/src/layouts/AuthLayout.tsx
@@ -4,12 +4,13 @@ import { Navigate } from "react-router";
 
 function AuthLayout() {
   const { data: user, isLoading, isError } = useAuth();
-
+  console.log(user, "user");
+  console.log(isError, "is error");
   if (isLoading) {
     return <></>;
   }
   if (isError || !user) {
-    <Navigate to="/login" replace />;
+    return <Navigate to="/login" replace />;
   }
 
   return <Outlet />;
